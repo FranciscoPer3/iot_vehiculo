05857fb58f160905ed3ecb174cd696bfa1b47d1f
FranciscoPerezPerez <l21200126@pachuca.tecnm.mx>
2025-12-03 09:19:01 -0600

    Actualizar URL ngro

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ class CarControl {
 
     connectWebSocket() {
         // ✅ URL ACTUALIZADA CON SERVEO
-        const serverUrl = 'wss://chilly-parents-show.loca.lt';
+        const serverUrl = 'wss://fickly-exorcismal-glen.ngrok-free.dev';
         
         
         console.log('Conectando a:', serverUrl);
